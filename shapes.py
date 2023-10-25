59e523f14e1dc8133ad90ad822159817b5c6eb07
Nikhil Jamdar <100251717+nikhiljamdar@users.noreply.github.com>
2023-10-25 21:34:30 +0530

    Update shapes.py

--- shapes.py
+++ shapes.py
@@ -1,7 +1,7 @@
 from turtle import Turtle , Screen
 import random
 tim = Turtle()
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "Wheat", "slateGray", "SeaGreen" ]
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "Wheat", "slateGray" ]
 
 def draw_shap(num_sides):
     angle = 360 / num_sides
